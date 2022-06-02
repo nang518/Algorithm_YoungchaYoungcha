f4aaf71f897811790e6ccf72d5a12cf46c352358
정나영 <jungnayoung@jeongnayeong-ui-MacBook-Pro.local>
2022-06-02 23:33:41 +0900

    new file

---
+++ 동전 0.py
@@ -0,0 +1,16 @@
+
+N,K = map(int, input().split())
+
+money = list()
+cnt = 0
+
+for i in range(0,N):
+    money.append(int(input()))
+
+for i in range(N-1,-1,-1):
+    if K >= money[i]:
+        cnt += K // money[i]
+        K %= money[i]
+    elif K == 0:
+        break
+print(cnt)
