e6abc8812c4d49c4c6eba32613cc5def3074aacd
nang518 <nang518@icloud.com>
2022-09-04 18:08:22 +0900

    22.09.04

---
+++ Python/Search/헌내기는 친구가 필요해.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n,m = map(int,input().split())
+
+graph = []
+for _ in range(n):
+    graph.append(list(input().strip()))
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+visited = [[False] * m for _ in range(n)]
+cnt = 0
+
+def dfs(x,y):
+    global cnt
+
+    visited[x][y] = True
+
+    if graph[x][y] == 'P':
+        cnt += 1
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+    
+        if 0<=nx<n and 0<=ny<m:
+            if not visited[nx][ny] and graph[nx][ny] != 'X':
+                dfs(nx,ny)
+    
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 'I':
+            dfs(i,j)
+            break
+
+if cnt == 0:
+    print('TT')
+else:
+    print(cnt)
