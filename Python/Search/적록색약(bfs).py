d85e2d1f827e541e3d965f838e6554c80feafef7
Nayoung Jung <nang518@icloud.com>
2022-09-27 18:21:22 +0900

    22.09.27

---
+++ Python/Search/적록색약(bfs).py
@@ -0,0 +1,17 @@
+target = 10
+m_list = [15,20,25,30,40,55,65,80]
+length = len(m_list)
+
+m_list.sort()
+left = 0 
+right = length-1
+
+while left<=right:
+    mid = (left+right)//2
+    if m_list[mid] == target:
+        print(mid+1)
+        break
+    elif m_list[mid]>target:
+        right = mid-1
+    else :
+        left = mid+1
