364c7ac8596752d5a145d40272b087b398895068
Nayoung Jung <nang518@icloud.com>
2022-11-08 02:06:05 +0900

    22.11.08

--- Python/Dynamic/가장 긴 증가하는 부분 수열.py
+++ Python/Dynamic/가장 긴 증가하는 부분 수열.py
@@ -1,10 +1,10 @@
-N = int(input())
-A = list(map(int,input().split()))
-dp = [1] * N
+n = int(input())
+a = list(map(int,input().split()))
+dp = [1]*n
 
-for i in range(1,N):
+for i in range(1,n):
     for j in range(i):
-        if A[i]>A[j]:
+        if a[i]>a[j]:
             dp[i] = max(dp[i],dp[j]+1)
 
 print(max(dp))
