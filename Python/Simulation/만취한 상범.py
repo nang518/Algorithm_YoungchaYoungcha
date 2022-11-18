e69a188eb935ac57b505fe4e12bf663c29dbdaf6
Nayoung Jung <nang518@icloud.com>
2022-11-18 20:07:33 +0900

    22.11.18

---
+++ Python/Simulation/만취한 상범.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    answer = 0
+    for i in range(1,n):
+        if i**2 <= n:
+            answer += 1
+        else: 
+            break
+    
+    print(answer)
