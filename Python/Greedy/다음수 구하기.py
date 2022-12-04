8e2c3bc8fbbd0b5c280363151065ab2000df23ca
Nayoung Jung <nang518@icloud.com>
2022-12-04 22:30:52 +0900

    22.12.04

---
+++ Python/Greedy/다음수 구하기.py
@@ -0,0 +1,26 @@
+t = int(input())
+for _ in range(t):
+    num = list(map(int,input()))  
+    index = 0
+    for i in range(len(num)-1, 0, -1):
+        if num[i] > num[i-1]:
+            if i == len(num) - 1:
+                index = len(num) - 2
+            else:
+                index = i - 1
+            break
+    
+    a = num[:index]
+    b = num[index:]
+    
+    if not a or not b:
+        print("BIGGEST")
+    else:
+        b.sort()
+        for i in range(len(b)):
+            if b[i] > num[index]:
+                a.append(b.pop(i))
+                a.extend(b)
+                break
+
+        print(''.join(map(str,a)))
