d5c41ab8eee3f10276acad057f1f266d6326ae35
Na Young Jung <nang518@icloud.com>
2023-03-22 11:07:43 +0900

    23.03.22

--- Python/Dynamic/1,2,3 더하기.py
+++ Python/Dynamic/1,2,3 더하기.py
@@ -1,22 +1,3 @@
-# import sys
-# input = sys.stdin.readline
-
-# T = int(input())
-
-# def dp(n):
-#     if n==1:
-#         return 1
-#     elif n==2:
-#         return 2
-#     elif n==3:
-#         return 4
-#     else:
-#         return dp(n-1) + dp(n-2) + dp(n-3)
-
-# for _ in range(T):
-#     n = int(input())
-#     print(dp(n))
-
 import sys
 input = sys.stdin.readline
 
