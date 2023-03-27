0d1d7ae75cc6b9d39e4e55bc18253701d449ba7e
Na Young Jung <nang518@icloud.com>
2023-03-27 23:13:30 +0900

    22.03.27

--- Python/Dynamic/2×n 타일링 2.py
+++ Python/Dynamic/2×n 타일링 2.py
@@ -5,6 +5,6 @@ n = int(input())
 
 dp = [0,1,3]
 for i in range(3, 1001):
-    dp.append(dp[i-1] + dp[i-2] *2)
+   dp.append(dp[i-1] + dp[i-2]*2)
 
 print(dp[n] % 10007)
--- Python/Dynamic/2×n 타일링.py
+++ Python/Dynamic/2×n 타일링.py
@@ -1,7 +1,18 @@
+# n = int(input())
+
+# dp = [0,1,2]
+# for i in range(3,1001):
+#     dp.append(dp[i-1]+dp[i-2])
+
+# print(dp[n]%10007)
+
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 dp = [0,1,2]
-for i in range(3,1001):
+for i in range(3, 1001):
     dp.append(dp[i-1]+dp[i-2])
 
-print(dp[n]%10007)
+print(dp[n] % 10007)
