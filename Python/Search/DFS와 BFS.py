7a1725b55aa2400c69cf87e049363f432402e4af
Na Young Jung <nang518@icloud.com>
2023-05-12 23:49:52 +0900

    23.05.12

--- Python/Search/DFS와 BFS.py
+++ Python/Search/DFS와 BFS.py
@@ -3,43 +3,44 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 from collections import deque
 
-n,m,v = map(int,input().split())
+#n: 정점 개수, m: 간선 개수, v: 시작 정점
+n,m,v = map(int,input().split()) 
 
-graph =[[] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    x,y = map(int,input().split())
-    graph[x].append(y)
-    graph[y].append(x)
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 for i in range(len(graph)):
     graph[i].sort()
 
 #dfs
-def dfs(num):
-    visited[num] = True
-    print(num, end=' ')
+def dfs(v):
+    visited[v] = True
+    print(v, end= ' ')
 
-    for i in graph[num]:
+    for i in graph[v]:
         if not visited[i]:
             visited[i] = True
             dfs(i)
 
 #bfs
-def bfs(num):
-    visited[num] = True
+def bfs(v):
+    visited[v] = True
 
     que = deque()
-    que.append(num)
+    que.append(v)
 
     while que:
         x = que.popleft()
-        print(x, end=' ')
+        print(x, end=' ') 
 
         for i in graph[x]:
             if not visited[i]:
                 visited[i] = True
                 que.append(i)
-        
+
 visited = [False] * (n+1)
 dfs(v)
 print()
