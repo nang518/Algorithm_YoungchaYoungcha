e21b9666e37a847e6adb66af700afdc4a8a28b68
Na Young Jung <nang518@icloud.com>
2023-05-19 21:49:09 +0900

    23.05.19

--- Python/Programmers/소수 찾기.py
+++ Python/Programmers/소수 찾기.py
@@ -20,7 +20,6 @@ def solution(numbers):
         elif is_prime(i):
             answer += 1
     
-    print(answer)
     return answer
 
 def is_prime(num):
@@ -29,26 +28,3 @@ def is_prime(num):
             return False
     
     return True
-
-solution("011")
-
-# def solution(numbers):
-#     answer = 0
-#     n = list(numbers)
-
-#     a = []
-#     for i in range(1, len(n)+1):
-#         a += list(permutations(n, i))   #경우의 수 반환
-
-#     b = []
-#     for i in a:
-#         b.append(int(''.join(i)))   
-#     b = list(set(b))    #경우의 수를 int형으로 담은 배열 b 선언
-
-#     for i in b:
-#         if i <= 1:
-#             continue    #1 이하면 무시
-#         elif is_prime(i):   # 소수면
-#             answer += 1     # answer 증가
-
-#     return answer
