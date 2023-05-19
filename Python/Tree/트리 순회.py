bc90b5b110fec852af39d78097fb27d1f0469f45
Na Young Jung <nang518@icloud.com>
2023-05-20 00:20:28 +0900

    23.05.19

--- Python/Tree/트리 순회.py
+++ Python/Tree/트리 순회.py
@@ -1,16 +1,3 @@
-# def inorder(node):
-#     if node == '.':
-#         return
-#     inorder(tree[node][0])
-#     print(node, end='')
-#     inorder(tree[node][1])
-
-# def postorder(node):
-#     if node == '.':
-#         return
-#     postorder(tree[node][0])
-#     postorder(tree[node][1])
-#     print(node, end='')
 import sys
 input = sys.stdin.readline
 
