2352e29d7445a412dbfc002f7a785f17cd301cd3
Na Young Jung <nang518@icloud.com>
2023-08-10 13:00:05 +0900

    23.08.10

--- Python/Dynamic/계단 오르기.py
+++ Python/Dynamic/계단 오르기.py
@@ -1,18 +1,16 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-N = int(input())
-floor = [0]
-for _ in range(N):
-    floor.append(int(input()))
+dp = [0]
+for _ in range(n):
+    dp.append(int(input()))
 
-if N == 1:
-    print(floor[1])
+if n==1:
+    print(dp[1])
 else:
-    score = [0] * (N+1)
-    score[1] = floor[1]
-    score[2] = floor[1] + floor[2]
-    for i in range(3,N+1):
-        score[i] = max(floor[i]+floor[i-1]+score[i-3], floor[i]+score[i-2])
-        
-    print(score[N])
+    score = [0] * (n+1)
+    score[1] = dp[1]
+    score[2] = dp[1] + dp[2]
+    for i in range(3,n+1):
+        score[i] = max(dp[i]+dp[i-1]+score[i-3], dp[i]+score[i-2])
+    
+    print(score[n])
