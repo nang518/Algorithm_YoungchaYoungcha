9893f524de3c8b722889f5187e44232d94fc980b
Na Young Jung <nang518@icloud.com>
2023-08-10 17:08:54 +0900

    23.08.10

--- Python/Dynamic/1,2,3 더하기.py
+++ Python/Dynamic/1,2,3 더하기.py
@@ -1,18 +1,8 @@
-import sys
-input = sys.stdin.readline
-
 t = int(input())
-
-def dp(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n== 3:
-        return 4
-    else:
-        return dp(n-1) + dp(n-2) + dp(n-3)
-
 for _ in range(t):
     n = int(input())
-    print(dp(n))
+    dp = [1,1,2]
+    for i in range(3,n+1):
+        dp.append(dp[i-1]+dp[i-2]+dp[i-3])
+
+    print(dp[n])
