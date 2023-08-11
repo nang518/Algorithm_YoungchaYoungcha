64226540f2f5344c2e2598335a58db5b294f2a2c
Na Young Jung <nang518@icloud.com>
2023-08-11 15:58:26 +0900

    23.08.11

--- Python/Search/섬의 개수.py
+++ Python/Search/섬의 개수.py
@@ -1,38 +1,37 @@
 import sys
+input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-input = sys.stdin.readline
-
-dx = [1,0,-1,0,-1,-1,1,1]
-dy = [0,1,0,-1,-1,1,-1,1]
+dx = [1,-1,0,0,-1,-1,1,1]
+dy = [0,0,1,-1,1,-1,1,-1]
 
 def dfs(x,y):
     visited[x][y] = True
-    
+
     for i in range(8):
         nx = x + dx[i]
         ny = y + dy[i]
-    
+
         if 0<=nx<h and 0<=ny<w:
-            if graph[nx][ny] == 1 and not visited[nx][ny]:
+            if graph[nx][ny]==1 and not visited[nx][ny]:
                 dfs(nx,ny)
 
 while True:
     w,h = map(int,input().split())
-    graph = []
-    visited = [[False] * w for _ in range(h)]
-    if w == 0 and h == 0:
+
+    if w==0 and h==0:
         break
+
+    graph = []
+    visited = [[False]*w for _ in range(h)]
+    for _ in range(h):
+        graph.append(list(map(int,input().split())))
     
     cnt = 0
-    for _ in range(h):
-        temp = list(map(int,input().split()))
-        graph.append(temp)
-    
     for i in range(h):
         for j in range(w):
             if graph[i][j] == 1 and not visited[i][j]:
                 dfs(i,j)
-                cnt+=1
-    
+                cnt += 1
+
     print(cnt)
--- Python/Search/트리의 부모 찾기.py
+++ Python/Search/트리의 부모 찾기.py
@@ -5,19 +5,19 @@ sys.setrecursionlimit(10**6)
 n = int(input())
 
 graph = [[] for _ in range(n+1)]
-for _ in range(n-1):
+for i in range(n-1): 
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(num):
-    visited[num] = True
+def dfs(n):
+    visited[n] = True
 
-    for i in graph[num]:
+    for i in graph[n]:
         if not visited[i]:
-            parent[i] = num
+            parent[i] = n
             dfs(i)
-            
+
 visited = [False] * (n+1)
 parent = [0] * (n+1)
 dfs(1)
