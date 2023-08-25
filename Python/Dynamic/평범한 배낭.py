2a18407178c7a4450d082d4760cb213750405b84
Na Young Jung <nang518@icloud.com>
2023-08-25 15:15:32 +0900

    23.08.25

--- Python/Dynamic/평범한 배낭.py
+++ Python/Dynamic/평범한 배낭.py
@@ -1,18 +1,16 @@
 n,k = map(int,input().split())
+w = [0]
+v = [0]
+for _ in range(n):
+    x,y = map(int,input().split())
+    w.append(x)
+    v.append(y)
 
 dp = [[0]*(k+1) for _ in range(n+1)]
-weight = [0]
-value = [0]
-
-for i in range(n):
-    w,h = map(int,input().split())
-    weight.append(w)
-    value.append(h)
-
 for i in range(1,n+1):
     for j in range(k,0,-1):
-        if j-weight[i] >= 0:
-            dp[i][j] = max(dp[i-1][j], value[i]+dp[i-1][j-weight[i]])
+        if j-w[i] >= 0:
+            dp[i][j] = max(dp[i-1][j], v[i]+dp[i-1][j-w[i]])
         else:
             dp[i][j] = dp[i-1][j]
 
