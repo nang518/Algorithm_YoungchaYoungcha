a47a4be8b0a5865bf94b16e8241984dfd4114527
nang518 <nang518@icloud.com>
2024-03-15 09:32:42 +0900

    24.03.15

--- Python/Implementation/2차원 배열의 합.py
+++ Python/Implementation/2차원 배열의 합.py
@@ -1,10 +1,7 @@
-import sys
-input = sys.stdin.readline 
-
 n,m = map(int, input().split())
 
 arr = []
-dp = [[0] * (m+1) for _ in range(n+1)]
+prefixSum = [[0] * (m+1) for _ in range(n+1)]
 
 for _ in range(n):
     tmp = list(map(int, input().split()))
@@ -12,13 +9,13 @@ for _ in range(n):
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
+        prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1] + arr[i-1][j-1]
 
 k = int(input())
 
 for _ in range(k):
     i,j,x,y = map(int, input().split())
 
-    result = dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1]
+    ans = prefixSum[x][y] - prefixSum[i-1][y] - prefixSum[x][j-1] + prefixSum[i-1][j-1]
 
-    print(result)
+    print(ans)
