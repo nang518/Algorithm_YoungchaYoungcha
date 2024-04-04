e43f513a5cd47d8e9c242e8ac6dbbb9b20ed01f8
nang518 <nang518@icloud.com>
2024-04-04 16:55:57 +0900

    24.04.04

--- Python/Backtracking/N과 M (2).py
+++ Python/Backtracking/N과 M (2).py
@@ -1,16 +1,16 @@
 n,m = map(int, input().split())
 
-arr = []
+arr =[]
 
-def backTracking():
+def backTracking(x):
     if len(arr) == m:
         print(' '.join(map(str, arr)))
         return
-
-    for i in range(1, n+1):
+    
+    for i in range(x, n+1):
         if i not in arr:
             arr.append(i)
-            backTracking()
+            backTracking(i+1)
             arr.pop()
 
-backTracking()
+backTracking(1)
