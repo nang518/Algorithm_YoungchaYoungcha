8f0e0e5b29e47f872847ba60873ad3708d50471c
nang518 <nang518@icloud.com>
2024-05-05 16:18:44 +0900

    24.05.05

---
+++ Python/Implementation/세 수.py
@@ -0,0 +1,25 @@
+a,b,c = map(int, input().split())
+
+if a + b == c:
+    print(a, '+', b, '=', c, sep = '')
+
+elif a - b == c:
+    print(a, '-', b, '=', c, sep = '')
+
+elif a * b == c:
+    print(a, '*', b, '=', c, sep = '')
+
+elif a / b == c:
+    print(a, '/', b, '=', c, sep = '')
+
+elif a == b + c:
+    print(a, '=', b, '+', c, sep = '')
+
+elif a == b - c:
+    print(a, '=', b, '-', c, sep = '')
+
+elif a == b * c:
+    print(a, '=', b, '*', c, sep = '')
+
+elif a == b / c:
+    print(a, '=', b, '/', c, sep = '')
