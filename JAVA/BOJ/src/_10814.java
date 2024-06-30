76e38085c05a52e0053fcc98e30684c316a11720
nang518 <nang518@icloud.com>
2024-06-30 20:41:37 +0900

    24.06.30

---
+++ JAVA/BOJ/src/_10814.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class _10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
