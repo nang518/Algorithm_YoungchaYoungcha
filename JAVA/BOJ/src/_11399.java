ea2e419a086e030df84134ced7789c73295285fc
nang518 <nang518@icloud.com>
2024-07-05 16:07:04 +0900

    24.07.05

--- JAVA/BOJ/src/_11399.java
+++ JAVA/BOJ/src/_11399.java
@@ -4,16 +4,26 @@ import java.util.*;
 public class _11399 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 배열 오름차순 정렬
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
 
         int sum = 0;
         int add = 0;
@@ -22,7 +32,6 @@ public class _11399 {
             add += arr[i];
             sum += add;
         }
-
-        System.out.print(sum);
+        System.out.println(sum);
     }
 }
