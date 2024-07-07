3225a265d4664b6f2f9088b0fe02a417da0faf7c
nang518 <nang518@icloud.com>
2024-07-07 19:56:51 +0900

    24.07.07

---
+++ JAVA/BOJ/src/_11650.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class _11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (x1, x2) -> {
+            if (x1[0] == x2[0]) {
+                return x1[1] - x2[1];
+            } else {
+                return x1[0] - x2[0];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
