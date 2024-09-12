891a4949a72f2a3153d55f60729e1917bdfdbcd2
nang518 <nang518@icloud.com>
2024-09-13 00:20:27 +0900

    24.09.13

---
+++ JAVA/BOJ/src/_15649.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class _15649 {
+    static int n,m;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visited = new boolean[n];
+
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    static void backTracking(int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                backTracking(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
