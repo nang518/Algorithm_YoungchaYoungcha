6870401f1706791a86f45ade5e06cf7db6d273d6
정나영 <nang518@icloud.com>
2024-09-30 15:40:26 +0900

    24.09.30

---
+++ JAVA/BOJ/src/_25206.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class _25206 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        double pointSum = 0;
+        double gradeSum = 0;
+
+        for (int i = 0; i < 20; i++) {
+            st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            double point = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
+            double score = 0;
+
+            if (!grade.equals("P")) {
+                switch (grade) {
+                    case "A+":
+                        score = 4.5;
+                        break;
+
+                    case "A0":
+                        score = 4.0;
+                        break;
+
+                    case "B+":
+                        score = 3.5;
+                        break;
+
+                    case "B0":
+                        score = 3.0;
+                        break;
+
+                    case "C+":
+                        score = 2.5;
+                        break;
+
+                    case "C0":
+                        score = 2.0;
+                        break;
+
+                    case "D+":
+                        score = 1.5;
+                        break;
+
+                    case "D0":
+                        score = 1.0;
+                        break;
+
+                    case "F":
+                        score = 0.0;
+                        break;
+                }
+
+                pointSum += point;
+                gradeSum += (score * point);
+            }
+        }
+
+        System.out.printf("%.6f", gradeSum / pointSum);
+
+    }
+}   
+
+
