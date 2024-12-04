dc1e3270473bca19d4f6cd3c86bb95e0558366b5
정나영 <nang518@icloud.com>
2024-12-05 04:14:04 +0900

    24.12.05

---
+++ JAVA/BOJ/src/_1926.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class _1926 {
+    static int n,m,cnt;
+    static int[][] graph;
+    static boolean[][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,1,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = 0, total = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && graph[i][j] == 1) {
+                    cnt = 0;
+                    dfs(i,j);
+                    if (cnt > result) result = cnt;
+                    total++;
+                }
+            }
+        }
+        sb.append(total + "\n" + result);
+        System.out.println(sb);
+    }
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+        cnt++;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (!visited[nx][ny] && graph[nx][ny] == 1) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+
+    }
+}
