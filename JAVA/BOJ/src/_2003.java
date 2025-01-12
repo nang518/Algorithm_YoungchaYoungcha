f310782f6e7e571f4a02321e0a13e2bd2e9f861a
정나영 <nang518@icloud.com>
2025-01-12 21:12:19 +0900

    25.01.12

---
+++ JAVA/BOJ/src/_2003.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class _2003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        int s = 0, e = 0, sum = 0, cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while (true) {
+            if (sum >= m) {
+                sum -= arr[s];
+                s++;
+            }
+            else if (e == n) break;
+            else {
+                sum += arr[e++];
+            }
+
+            if (sum == m) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
