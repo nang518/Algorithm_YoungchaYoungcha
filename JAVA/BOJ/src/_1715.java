0687432d6c7d1e3ee5f731f06dd9533b3a83e4a7
정나영 <nang518@icloud.com>
2025-03-17 00:04:40 +0900

    25.03.16

---
+++ JAVA/BOJ/src/_1715.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class _1715 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
+
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        while (pq.size() != 2) {
+            int x = pq.poll() + pq.poll();
+            result += x;
+            pq.offer(x);
+        }
+
+        result += pq.poll() + pq.poll();
+
+        System.out.println(result);
+    }
+}
