8035fb94357e952282ac5e8cc444e97fd3d718a9
정나영 <nang518@icloud.com>
2025-05-24 03:12:29 +0900

    25.05.24

--- JAVA/BOJ/src/_11279.java
+++ JAVA/BOJ/src/_11279.java
@@ -1,32 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class _11279 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
+        while (n-- > 0) {
             int x = Integer.parseInt(br.readLine());
 
             if (x == 0) {
-                if (pq.isEmpty()) {
-                    sb.append("0");
-                } else {
-                    sb.append(pq.poll());
-                }
-                sb.append('\n');
+                if (pq.isEmpty()) sb.append(0).append('\n');
+                else sb.append(pq.poll()).append('\n');
             }
 
-            else {
-                pq.offer(x);
-            }
+            else pq.add(x);
         }
 
         System.out.println(sb);
