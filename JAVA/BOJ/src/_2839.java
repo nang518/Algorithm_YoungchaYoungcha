dc60253b2f03c57b0250cc79ca459e8640bb02eb
정나영 <nang518@icloud.com>
2025-08-08 23:24:23 +0900

    25.08.08

--- JAVA/BOJ/src/_2839.java
+++ JAVA/BOJ/src/_2839.java
@@ -3,23 +3,15 @@ import java.io.*;
 public class _2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 4 || n == 7) {
-            System.out.println(-1);
-        }
+        if (n == 4 || n == 7) sb.append(-1).append('\n');
+        else if (n % 5 == 0) sb.append(n/5).append('\n');
+        else if (n % 5 == 1 || n % 5 == 3) sb.append((n/5)+1).append('\n');
+        else if (n % 5 == 2 || n % 5 == 4) sb.append((n/5)+2).append('\n');
 
-        else if (n % 5 == 0) {
-            System.out.println(n / 5);
-        }
-
-        else if (n % 5 == 1 || n % 5 == 3) {
-            System.out.println((n /5 ) + 1);
-
-        }
-
-        else if (n % 5 == 2 || n % 5 == 4) {
-            System.out.println((n / 5) + 2);
-        }
+        System.out.println(sb);
     }
 }
