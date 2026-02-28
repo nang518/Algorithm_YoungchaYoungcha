898c4229bc4d0d14a2d479892dbe38db244d7e36
nang518 <nang518@icloud.com>
2026-02-28 13:09:59 +0900

    26.02.28

---
+++ JAVA/BOJ/src/_2960.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class _2960 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+
+        for (int i = 2; i <= n; i++) arr[i] = i;
+
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) continue;
+
+            for (int j = i; j <= n; j += i) {
+                if (arr[j] != 0) {
+                    arr[j] = 0;
+                    k--;
+                }
+
+                if (k == 0) {
+                    System.out.println(j);
+                    return;
+                }
+            }
+        }
+    }
+}
